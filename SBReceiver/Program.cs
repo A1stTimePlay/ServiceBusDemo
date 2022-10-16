1d9bac9d935edf09435afccd2984d8de3c2cbeb5
Nguyen An <lanehacker7294@yahoo.com>
2022-10-17 02:07:04 +0700

    Add dead letter queue demo

--- SBReceiver/Program.cs
+++ SBReceiver/Program.cs
@@ -1,17 +1,15 @@
 ﻿using Microsoft.Azure.ServiceBus;
 using SBShared.Models;
-using System;
 using System.Text;
 using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SBReceiver
 {
     class Program
     {
-        const string connectionString = "Endpoint=sb://anservicebusdemo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=D5hATU7XhFfJ2s9TYHQl0nJskNJgT/8M1F9rOiqRRTg=";
-        const string queueName = "PersonQueue";
+        const string connectionString = "Endpoint=sb://xuanan.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=IhG/ARHE9f6palXyqsM9UusOCrq+w0bq6iuF1dgP+Hc=";
+        const string queueName = "simplequeue";
         static IQueueClient queueClient;
 
         static async Task Main(string[] args)
@@ -35,9 +33,17 @@ namespace SBReceiver
         {
             var jsonString = Encoding.UTF8.GetString(message.Body);
             PersonModel person = JsonSerializer.Deserialize<PersonModel>(jsonString);
-            Console.WriteLine($"Person Received: {person.FirstName} {person.LastName}");
-
-            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+            Console.WriteLine($"Message received: First name: {person.FirstName} - Last name: {person.LastName}");
+            if (Regex.IsMatch(person.FirstName, @"^[A-Za-z\s]*$") && Regex.IsMatch(person.LastName, @"^[A-Za-z\s]*$"))
+            {
+                Console.WriteLine($"Message process success");
+                await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+            }
+            else
+            {
+                Console.WriteLine($"Message process fail");
+                await queueClient.AbandonAsync(message.SystemProperties.LockToken);
+            }
         }
 
         private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
